9b38803cfe56d9946e7fc6f7540c8c9cd900a095
zonoise <zonoise@gmail.com>
2014-09-02 00:40:45 +0900

    Create autoload.php

---
+++ src/autoload.php
@@ -0,0 +1,14 @@
+spl_autoload_register(
+    function($class) {
+        static $classes = null;
+        if ($classes === null) {
+            $classes = array(
+                'Charactor' => '/Charactor.php',
+            );
+        }
+        $cn = strtolower($class);
+        if (isset($classes[$cn])) {
+            require __DIR__ . $classes[$cn];
+        }
+    }
+);
