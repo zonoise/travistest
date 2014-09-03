f62898794cffe4567be5f3ea33093b31e8b1d225
zonoise <zonoise@gmail.com>
2014-09-04 02:10:44 +0900

    fix typo

--- tests/CharactorTest.php
+++ tests/CharactorTest.php
@@ -8,7 +8,7 @@ class CharactorTest extends PHPUnit_Framework_TestCase
   
   public function testHasAttributes()
   {
-    $attributes=['hp','bp','atk','def']
+    $attributes=['hp','bp','atk','def'];
     foreach($attributes as $attribute){
       $this->assertClassHasAttribute($attribute,'Charactor');
     }
